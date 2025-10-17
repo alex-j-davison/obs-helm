594f8e436bd99c83c611f2b05466aa843e3f7155
Alex Davison <alexd@splunk.com>
2025-10-17 22:03:15 +0100

    Update shell

--- installhelm.sh
+++ installhelm.sh
@@ -7,17 +7,32 @@ echo "# Version: 0.0.1                                              #"
 echo "# Starting...                                                 #"
 echo "###############################################################"
 echo ""
-
+echo "##############################"
+echo "# Update according to Github #"
+echo "##############################"
+echo ""
+echo "Step 1/5:"
 git branch --set-upstream-to=origin/main backup-main
-
+echo "Step 2/5:"
 git fetch --all
-
+echo "Step 3/5:"
 git checkout -b backup-main
-
+echo "Step 4/5:"
 git reset --hard origin/main
-
+echo "Step 5/5:"
 git pull
-
+echo ""
+echo "###########################"
+echo "# Update file permissions #"
+echo "###########################"
+echo ""
+echo "Step 1/1: Update permissions"
+chmod +x installhelm.sh
+echo ""
+echo "###############"
+echo "# Update helm #"
+echo "###############"
+echo ""
 if [ -e SMEObs1.yaml ]
 then
     echo "Updating helm"
