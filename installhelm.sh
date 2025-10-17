d3502cb666e1657a3d31fba450680fc88972223b
Alex Davison <alexd@splunk.com>
2025-10-17 21:18:44 +0100

    Initial commit

---
+++ installhelm.sh
@@ -0,0 +1,10 @@
+echo "###############################################################"
+echo "# Script: Step-1 Setup observability instance                 #"
+echo "# Description: To create a observability instance.            #"
+echo "# Created date: 17/10/25                                      #"
+echo "# Arthur: Alex J Davison (alexdav@cisco.com)                  #"
+echo "# Version: 0.0.1                                              #"
+echo "# Starting...                                                 #"
+echo "###############################################################"
+echo ""
+
