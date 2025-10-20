9d41491580d09d435078b2d241e1e06a6ed0ea0c
Alex Davison <alexd@splunk.com>
2025-10-20 16:53:58 +0100

    Update if

--- installhelm.sh
+++ installhelm.sh
@@ -13,75 +13,76 @@ echo "# Initialization #"
 echo "##################"
 echo ""
 if [ $# -eq 0 ]
-  then
-    echo "Error: No arguments supplied"
-    set -e
-fi
-file=$1
-echo "YAML:" ${file}
-echo ""
-echo "#################"
-echo "# Starting loop #"
-echo "#################"
-echo ""
-
-pasttime=`ls -ltr | grep "${file}.yaml"`
-
-while true
-do
-    echo "##############################"
-    echo "# Update according to Github #"
-    echo "##############################"
-    echo ""
-    echo "Step 1/5: Set branch 'backup-main' set up to track remote branch 'main' from 'origin'"
-    git branch --set-upstream-to=origin/main backup-main
-    echo "Step 2/5: Update all origin/<branch> refs to latest:"
-    git fetch --all
-    echo "Step 3/5: Backup your current branch"
-    git checkout -b backup-main
-    echo "Step 4/5: Jump to the latest commit on origin/main and checkout those files"
-    git reset --hard origin/main
-    echo "Step 5/5: Pull latest"
-    git pull
-    echo ""
-    echo "###########################"
-    echo "# Update file permissions #"
-    echo "###########################"
-    echo ""
-    echo "Step 1/1: Update permissions"
-    chmod +x installhelm.sh
-    echo ""
-    echo "###############"
-    echo "# Update helm #"
-    echo "###############"
-    echo ""
-    
-    echo "Step 1/4: Get file details" 
-    currenttime=`ls -ltr | grep "${file}.yaml"`
-    
-    echo "Step 2/4: Check if ${file}.yaml exists" 
-    if [ -e ${file}.yaml ]
     then
-        echo "Step 3/4: Check if ${file}.yaml has changed" 
-        echo "C: $currenttime"
-        echo "P: $pasttime"
-        if [ "$currenttime" = "$pasttime" ];
-        then
-            echo "Step 4/4: Upgrade not possible, no change"
-        else
-            echo "Step 4/4: Upgrade helm"
-            sudo microk8s helm upgrade splunk-otel-collector --values ${file}.yaml splunk-otel-collector-chart/splunk-otel-collector
-            pasttime=$currenttime
-            
-        fi
+        echo "Error: No arguments supplied"
+        set -e
     else
-        echo "Step 3/4: Upgrade not possible, no yaml"
-    fi
-    echo ""
-    total_seconds=15
-    while [ $total_seconds -gt 0 ]; do
-        printf "Time remaining: %d seconds\r" "$total_seconds"
-        sleep 1
-        total_seconds=$((total_seconds - 1))
-    done
-done
+        file=$1
+        echo "YAML:" ${file}
+        echo ""
+        echo "#################"
+        echo "# Starting loop #"
+        echo "#################"
+        echo ""
+
+        pasttime=`ls -ltr | grep "${file}.yaml"`
+
+        while true
+        do
+            echo "##############################"
+            echo "# Update according to Github #"
+            echo "##############################"
+            echo ""
+            echo "Step 1/5: Set branch 'backup-main' set up to track remote branch 'main' from 'origin'"
+            git branch --set-upstream-to=origin/main backup-main
+            echo "Step 2/5: Update all origin/<branch> refs to latest:"
+            git fetch --all
+            echo "Step 3/5: Backup your current branch"
+            git checkout -b backup-main
+            echo "Step 4/5: Jump to the latest commit on origin/main and checkout those files"
+            git reset --hard origin/main
+            echo "Step 5/5: Pull latest"
+            git pull
+            echo ""
+            echo "###########################"
+            echo "# Update file permissions #"
+            echo "###########################"
+            echo ""
+            echo "Step 1/1: Update permissions"
+            chmod +x installhelm.sh
+            echo ""
+            echo "###############"
+            echo "# Update helm #"
+            echo "###############"
+            echo ""
+            
+            echo "Step 1/4: Get file details" 
+            currenttime=`ls -ltr | grep "${file}.yaml"`
+            
+            echo "Step 2/4: Check if ${file}.yaml exists" 
+            if [ -e ${file}.yaml ]
+            then
+                echo "Step 3/4: Check if ${file}.yaml has changed" 
+                echo "C: $currenttime"
+                echo "P: $pasttime"
+                if [ "$currenttime" = "$pasttime" ];
+                then
+                    echo "Step 4/4: Upgrade not possible, no change"
+                else
+                    echo "Step 4/4: Upgrade helm"
+                    sudo microk8s helm upgrade splunk-otel-collector --values ${file}.yaml splunk-otel-collector-chart/splunk-otel-collector
+                    pasttime=$currenttime
+                    
+                fi
+            else
+                echo "Step 3/4: Upgrade not possible, no yaml"
+            fi
+            echo ""
+            total_seconds=15
+            while [ $total_seconds -gt 0 ]; do
+                printf "Time remaining: %d seconds\r" "$total_seconds"
+                sleep 1
+                total_seconds=$((total_seconds - 1))
+            done
+        done
+fi
